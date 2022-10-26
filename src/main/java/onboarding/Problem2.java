ddee036a549a868da4d1d6d43ce6d546fe861b25
dwl21 <nggus5@naver.com>
2022-10-26 17:37:16 +0900

    Solve Problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (!cryptogram.equals(distinct(cryptogram))) {
+            cryptogram = distinct(cryptogram);
+        }
+        return cryptogram;
+    }
+
+    private static String distinct(String cryptogram) {
+        for (int i = 0; i < 26; i++) {
+            String alphabet = String.valueOf((char) ('a' + i));
+            cryptogram = cryptogram.replaceAll(alphabet + "{2,}", "");
+        }
+        return cryptogram;
     }
 }
